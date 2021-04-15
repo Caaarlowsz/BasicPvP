5f7d5efe3ca07dc45b7d7e62aacebfd79f85183b
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-15 10:22:28 -0300

    Adicionando classe principal.

---
+++ src/com/github/caaarlowsz/basicpvp/BasicKitPvP.java
@@ -0,0 +1,16 @@
+package com.github.caaarlowsz.basicpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BasicKitPvP extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+}

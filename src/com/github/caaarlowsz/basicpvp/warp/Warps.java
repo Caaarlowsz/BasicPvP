81c5c599fbbb1af369505007e12e2bdcb13e9834
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-16 11:56:43 -0300

    Adicionando warp default.

--- src/com/github/caaarlowsz/basicpvp/warp/Warps.java
+++ src/com/github/caaarlowsz/basicpvp/warp/Warps.java
@@ -2,13 +2,19 @@ package com.github.caaarlowsz.basicpvp.warp;
 
 import java.util.ArrayList;
 
+import com.github.caaarlowsz.basicpvp.warp.warps.SpawnWarp;
+
 public final class Warps {
 
 	private static final ArrayList<Warp> warps = new ArrayList<>();
-	private static final Warp NONE_WARP = new Warp("Nenhuma");
+	private static final Warp NONE_WARP = new Warp("Nenhuma"), DEFAULT_WARP = new SpawnWarp();
 
 	public static Warp getNoneWarp() {
 		return NONE_WARP;
+	}
+
+	public static Warp getDefaultWarp() {
+		return DEFAULT_WARP;
 	}
 
 	public static ArrayList<Warp> getWarps() {

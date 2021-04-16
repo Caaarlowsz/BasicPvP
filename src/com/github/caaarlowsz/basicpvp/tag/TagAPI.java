5dc4f16cfb81309f9635585f7a59a9e625df7a88
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-16 19:56:38 -0300

    Adicionado classe api para manipular as tags.

---
+++ src/com/github/caaarlowsz/basicpvp/tag/TagAPI.java
@@ -0,0 +1,30 @@
+package com.github.caaarlowsz.basicpvp.tag;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
+import com.github.caaarlowsz.basicpvp.files.StatusFile;
+import com.nametagedit.plugin.NametagEdit;
+
+public final class TagAPI {
+
+	private static final HashMap<UUID, Tag> tagMap = new HashMap<>();
+
+	public static Tag getTag(Player player, Tag tag) {
+		return tagMap.getOrDefault(player.getUniqueId(), Tag.getDefaultTag());
+	}
+
+	public static void setTag(Player player, Tag tag) {
+		tagMap.put(player.getUniqueId(), tag);
+		NametagEdit.getApi().setNametag(player.getName(), tag.getPrefix(),
+				" §8(" + StatusFile.getRankIcon(player) + "§8)");
+		player.setDisplayName(tag.getPrefix() + player.getName() + " §8(" + StatusFile.getRankIcon(player) + "§8)");
+	}
+
+	public static void removeTag(Player player) {
+		NametagEdit.getApi().clearNametag(player.getName());
+		tagMap.remove(player.getUniqueId());
+	}
+}

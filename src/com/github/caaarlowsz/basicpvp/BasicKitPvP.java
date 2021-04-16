eb7cabe2ac441b013bb9ace68aac07d23184ec6e
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-16 11:55:40 -0300

    Instanciando classe Warps.

--- src/com/github/caaarlowsz/basicpvp/BasicKitPvP.java
+++ src/com/github/caaarlowsz/basicpvp/BasicKitPvP.java
@@ -1,6 +1,11 @@
 package com.github.caaarlowsz.basicpvp;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.caaarlowsz.basicpvp.utils.Strings;
+import com.github.caaarlowsz.basicpvp.warp.Warps;
 
 public final class BasicKitPvP extends JavaPlugin {
 
@@ -9,13 +14,27 @@ public final class BasicKitPvP extends JavaPlugin {
 	}
 
 	@Override
+	public void onLoad() {
+		super.onLoad();
+	}
+
+	@Override
 	public void onEnable() {
 		super.onEnable();
 		this.saveDefaultConfig();
+
+		new Warps();
+
+		Bukkit.getConsoleSender().sendMessage(Strings.getPrefixo() + " §aPlugin habilitado com sucesso.");
 	}
 
 	@Override
 	public void onDisable() {
 		super.onDisable();
+		HandlerList.unregisterAll(this);
+		Bukkit.getScheduler().cancelTasks(this);
+		Bukkit.getOnlinePlayers()
+				.forEach(players -> players.kickPlayer(Strings.getPrefixo() + " §cServidor reiniciando..."));
+		Bukkit.getConsoleSender().sendMessage(Strings.getPrefixo() + " §cPlugin desabilitado com sucesso.");
 	}
 }

f0f786b669823aacd67676374b9ef85fc4249706
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-16 22:03:00 -0300

    Adicionando comando /report.

--- src/com/github/caaarlowsz/basicpvp/apis/PlayerAPI.java
+++ src/com/github/caaarlowsz/basicpvp/apis/PlayerAPI.java
@@ -1,6 +1,7 @@
 package com.github.caaarlowsz.basicpvp.apis;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 import org.bukkit.entity.Player;
@@ -8,6 +9,7 @@ import org.bukkit.entity.Player;
 public final class PlayerAPI {
 
 	private static final ArrayList<UUID> tellOff = new ArrayList<>();
+	private static final HashMap<UUID, Long> useReport = new HashMap<>();
 
 	public static boolean hasTellOff(Player player) {
 		return tellOff.contains(player.getUniqueId());
@@ -20,5 +22,23 @@ public final class PlayerAPI {
 
 	public static void removeTellOff(Player player) {
 		tellOff.remove(player.getUniqueId());
+	}
+
+	public static boolean hasUseReport(Player player) {
+		return useReport.containsKey(player.getUniqueId());
+	}
+
+	public static long getUseReport(Player player) {
+		if (hasUseReport(player))
+			return (useReport.get(player.getUniqueId()) - System.currentTimeMillis()) / 1000L;
+		return 0L;
+	}
+
+	public static void setUseReport(Player player, long seconds) {
+		useReport.put(player.getUniqueId(), System.currentTimeMillis() + (seconds * 1000L));
+	}
+
+	public static void removeUseReport(Player player) {
+		useReport.remove(player.getUniqueId());
 	}
 }
---
+++ src/com/github/caaarlowsz/basicpvp/commands/ReportCommand.java
@@ -0,0 +1,57 @@
+package com.github.caaarlowsz.basicpvp.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.github.paperspigot.Title;
+
+import com.github.caaarlowsz.basicpvp.apis.PlayerAPI;
+import com.github.caaarlowsz.basicpvp.utils.Strings;
+
+public final class ReportCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if (PlayerAPI.getUseReport(player) <= 0) {
+				if (args.length > 1) {
+					Player target = Bukkit.getPlayer(args[0]);
+					if (target != null) {
+						String reason = "";
+						for (int i = 1; i < args.length; i++)
+							reason += (reason.isEmpty() ? "" : " ") + args[i];
+
+						String finalReason = reason;
+						for (Player staffers : Bukkit.getOnlinePlayers()) {
+							if (staffers.hasPermission("kitpvp.command.report")) {
+								staffers.playSound(staffers.getLocation(), Sound.LEVEL_UP, 15F, 1F);
+								staffers.sendMessage(" ");
+								staffers.sendMessage("  §a§lNOVA DENUNCIA!");
+								staffers.sendMessage(" ");
+								staffers.sendMessage(" §aReporter: §f" + player.getName());
+								staffers.sendMessage(" ");
+								staffers.sendMessage(" §aReportado: §f" + target.getName());
+								staffers.sendMessage(" §aMotivo: §f" + finalReason);
+								staffers.sendMessage(" ");
+								player.sendTitle(new Title("§a§lNOVA DENUNCIA", "§fNova denúncia!"));
+							}
+						}
+
+						PlayerAPI.setUseReport(player, 15);
+						player.sendMessage(Strings.getPrefixo() + " §aVocê denunciou " + target.getName() + " por "
+								+ reason + ".");
+					} else
+						player.sendMessage(Strings.getPrefixo() + " §cO seu alvo não foi encontrado.");
+				} else
+					player.sendMessage(Strings.getPrefixo() + " §7Use: /" + label + " (Nickname) (Motivo)");
+			} else
+				player.sendMessage(Strings.getPrefixo() + " §cAguarde para reportar novamente.");
+		} else
+			sender.sendMessage(Strings.getPrefixo() + " §cApenas Jogadores podem usar este Comando.");
+		return true;
+	}
+}

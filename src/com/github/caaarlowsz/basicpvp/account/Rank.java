092bc387c77383b23217ba8680c3106014259048
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-19 11:14:36 -0300

    Adicionando enum de ranks.

---
+++ src/com/github/caaarlowsz/basicpvp/account/Rank.java
@@ -0,0 +1,59 @@
+package com.github.caaarlowsz.basicpvp.account;
+
+import org.bukkit.ChatColor;
+
+public enum Rank {
+	LEGENDARY(ChatColor.DARK_RED, '✪', 300),
+	SAPPHIRE(ChatColor.DARK_AQUA, '✵', 2700),
+	RUBY(ChatColor.RED, '✸', 2400),
+	EMERALD(ChatColor.DARK_GREEN, '✶', 2100),
+	DIAMOND(ChatColor.AQUA, '❋', 1600),
+	GOLD(ChatColor.GOLD, '✦', 1500),
+	SILVER(ChatColor.GRAY, '✷', 1200),
+	EXPERT(ChatColor.DARK_BLUE, '✥', 900),
+	ADVANCED(ChatColor.YELLOW, '☲', 600),
+	PRIMARY(ChatColor.GREEN, '═', 300),
+	UNRANKED(ChatColor.WHITE, '-', 0);
+
+	private final ChatColor color;
+	private final char symbol;
+	private final int neededXP;
+
+	private Rank(ChatColor color, char symbol, int neededXP) {
+		this.color = color;
+		this.symbol = symbol;
+		this.neededXP = neededXP;
+	}
+
+	public String getName() {
+		return this.name();
+	}
+
+	public ChatColor getColor() {
+		return this.color;
+	}
+
+	public char getSymbol() {
+		return this.symbol;
+	}
+
+	public int getNeededXP() {
+		return this.neededXP;
+	}
+
+	public String getColoredSymbol() {
+		return this.getColor() + String.valueOf(this.getSymbol());
+	}
+
+	public String getColoredSymbolName() {
+		return this.getColor() + String.valueOf(this.getSymbol()) + " " + this.getName();
+	}
+
+	public static Rank getRankByXP(int xp) {
+		for (Rank rank : values()) {
+			if (xp >= rank.getNeededXP())
+				return rank;
+		}
+		return null;
+	}
+}

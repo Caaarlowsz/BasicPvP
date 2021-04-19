ef91e824c04141d093e35f7a072d473f01f1bf22
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-19 17:03:31 -0300

    Modificando a estrutura do código.

--- src/com/github/caaarlowsz/basicpvp/kit/KitListeners.java
+++ src/com/github/caaarlowsz/basicpvp/kit/KitListeners.java
@@ -1,6 +1,7 @@
 package com.github.caaarlowsz.basicpvp.kit;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,14 +16,14 @@ public final class KitListeners implements Listener {
 		Player player = event.getPlayer();
 
 		if (KitAPI.hasKit(player) && event.getMaterial() == Material.COMPASS) {
-			player.getNearbyEntities(100D, 150D, 100D).stream().filter(
-					entity -> entity instanceof Player && player.getLocation().distance(entity.getLocation()) > 10D)
-					.forEach(entity -> {
-						Player target = (Player) entity;
-						player.setCompassTarget(target.getLocation());
-						player.sendMessage(Strings.getPrefixo() + " §eBússola apontando para: " + target.getName());
-						return;
-					});
+			for (Entity entity : player.getNearbyEntities(100D, 150D, 100D)) {
+				if (entity instanceof Player && player.getLocation().distance(entity.getLocation()) > 10D) {
+					Player target = (Player) entity;
+					player.setCompassTarget(target.getLocation());
+					player.sendMessage(Strings.getPrefixo() + " §eBússola apontando para: " + target.getName());
+					return;
+				}
+			}
 
 			player.setCompassTarget(player.getLocation());
 			player.sendMessage(Strings.getPrefixo() + " §cNenhum alvo foi encontrado.");

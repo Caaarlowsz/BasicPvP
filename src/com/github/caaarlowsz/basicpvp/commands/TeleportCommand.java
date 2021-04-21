38fe797454c56912700c4affc342b6eea5f722ed
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-21 11:11:02 -0300

    Corrigindo bug de código morto no comando /teleport.

--- src/com/github/caaarlowsz/basicpvp/commands/TeleportCommand.java
+++ src/com/github/caaarlowsz/basicpvp/commands/TeleportCommand.java
@@ -16,47 +16,43 @@ public class TeleportCommand implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			if (player.hasPermission("kitpvp.command.teleport")) {
-				if (args.length > 0) {
-					if (args.length > 2) {
+				if (args.length > 2) {
+					try {
+						double x = Integer.parseInt(args[0]);
 						try {
-							double x = Integer.parseInt(args[0]);
+							double y = Integer.parseInt(args[1]);
 							try {
-								double y = Integer.parseInt(args[1]);
-								try {
-									double z = Integer.parseInt(args[2]);
-									player.teleport(new Location(player.getWorld(), x + 0.5, y + 0.5, z + 0.5));
-									player.sendMessage(
-											Strings.getPrefixo() + " §aVocê se teleportou até as coordenadas " + x
-													+ ", " + y + ", " + z + ".");
-								} catch (NumberFormatException ex) {
-									player.sendMessage(Strings.getPrefixo() + " §cO valor Z precisa ser numérico.");
-								}
+								double z = Integer.parseInt(args[2]);
+								player.teleport(new Location(player.getWorld(), x + 0.5, y + 0.5, z + 0.5));
+								player.sendMessage(Strings.getPrefixo() + " §aVocê se teleportou até as coordenadas "
+										+ x + ", " + y + ", " + z + ".");
 							} catch (NumberFormatException ex) {
-								player.sendMessage(Strings.getPrefixo() + " §cO valor Y precisa ser numérico.");
+								player.sendMessage(Strings.getPrefixo() + " §cO valor Z precisa ser numérico.");
 							}
 						} catch (NumberFormatException ex) {
-							player.sendMessage(Strings.getPrefixo() + " §cO valor X precisa ser numérico.");
+							player.sendMessage(Strings.getPrefixo() + " §cO valor Y precisa ser numérico.");
 						}
-					} else if (args.length > 1) {
-						Player target = Bukkit.getPlayer(args[0]);
-						if (target != null) {
-							if (args.length == 2) {
-								Player target2 = Bukkit.getPlayer(args[1]);
-								if (target2 != null) {
-									target.teleport(target2.getLocation());
-									player.sendMessage(Strings.getPrefixo() + " §aVocê teleportou " + target.getName()
-											+ " para " + target2.getName() + ".");
-								} else
-									player.sendMessage(
-											Strings.getPrefixo() + " §cO seu segundo alvo não foi encontrado.");
-							} else {
-								player.teleport(target.getLocation());
-								player.sendMessage(
-										Strings.getPrefixo() + " §aVocê se teleportou até " + target.getName() + ".");
-							}
-						} else
-							player.sendMessage(Strings.getPrefixo() + " §cO seu alvo não foi encontrado.");
+					} catch (NumberFormatException ex) {
+						player.sendMessage(Strings.getPrefixo() + " §cO valor X precisa ser numérico.");
 					}
+				} else if (args.length > 0) {
+					Player target = Bukkit.getPlayer(args[0]);
+					if (target != null) {
+						if (args.length == 2) {
+							Player target2 = Bukkit.getPlayer(args[1]);
+							if (target2 != null) {
+								target.teleport(target2.getLocation());
+								player.sendMessage(Strings.getPrefixo() + " §aVocê teleportou " + target.getName()
+										+ " para " + target2.getName() + ".");
+							} else
+								player.sendMessage(Strings.getPrefixo() + " §cO seu segundo alvo não foi encontrado.");
+						} else {
+							player.teleport(target.getLocation());
+							player.sendMessage(
+									Strings.getPrefixo() + " §aVocê se teleportou até " + target.getName() + ".");
+						}
+					} else
+						player.sendMessage(Strings.getPrefixo() + " §cO seu alvo não foi encontrado.");
 				} else
 					player.sendMessage(Strings.getPrefixo() + " §7Use: /" + label + " (Nickname) [Nickname] ou /"
 							+ label + " (X) (Y) (Z)");

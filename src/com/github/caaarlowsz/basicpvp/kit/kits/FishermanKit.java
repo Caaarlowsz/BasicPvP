f94b738f9402ede73e387004acc4e6069fdc23d5
Caaarlowsz <caaarlowsz@outlook.com>
2021-04-29 16:23:24 -0300

    Adicionando vara de pesca ao inventário.

--- src/com/github/caaarlowsz/basicpvp/kit/kits/FishermanKit.java
+++ src/com/github/caaarlowsz/basicpvp/kit/kits/FishermanKit.java
@@ -1,19 +1,35 @@
 package com.github.caaarlowsz.basicpvp.kit.kits;
 
+import java.util.Arrays;
+
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.PlayerInventory;
 
 import com.github.caaarlowsz.basicpvp.apis.WorldGuardAPI;
 import com.github.caaarlowsz.basicpvp.kit.Kit;
 import com.github.caaarlowsz.basicpvp.kit.KitAPI;
+import com.github.caaarlowsz.basicpvp.utils.Stacks;
 import com.github.caaarlowsz.basicpvp.utils.Strings;
 
 public final class FishermanKit extends Kit implements Listener {
 
 	public FishermanKit() {
 		super("Fisherman");
+	}
+
+	@Override
+	public void giveItems(Player player) {
+		PlayerInventory inv = player.getInventory();
+		super.giveItems(player);
+
+		inv.setItem(1, Stacks.item(Material.FISHING_ROD, true, Arrays.asList(ItemFlag.HIDE_UNBREAKABLE),
+				"§aVara de Pesca", "§7Kit " + this.getName()));
+		player.updateInventory();
 	}
 
 	@EventHandler

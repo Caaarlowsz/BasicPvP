37ef2748df7f3e4dddfc7b4d178dd0f9652f41f6
Caaarlowsz <caaarlowsz@outlook.com>
2021-05-01 16:30:55 -0300

    Adicionando o Kit Poseidon.

---
+++ src/com/github/caaarlowsz/basicpvp/kit/kits/PoseidonKit.java
@@ -0,0 +1,30 @@
+package com.github.caaarlowsz.basicpvp.kit.kits;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import com.github.caaarlowsz.basicpvp.apis.WorldGuardAPI;
+import com.github.caaarlowsz.basicpvp.kit.Kit;
+import com.github.caaarlowsz.basicpvp.kit.KitAPI;
+
+public final class PoseidonKit extends Kit implements Listener {
+
+	public PoseidonKit() {
+		super("Poseidon");
+	}
+
+	@EventHandler
+	private void onPlayerMove(PlayerMoveEvent event) {
+		Player player = event.getPlayer();
+		if (KitAPI.getKit(player) instanceof PoseidonKit && WorldGuardAPI.hasPvP(player)
+				&& event.getTo().getBlock().getType().name().contains("WATER")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 100, 0), true);
+			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 100, 1), true);
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100, 1), true);
+		}
+	}
+}

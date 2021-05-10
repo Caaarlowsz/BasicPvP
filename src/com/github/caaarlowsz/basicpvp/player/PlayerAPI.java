317a4293ec5f1bcd4e365d9292ba0e5ae52a2e11
Caaarlowsz <caaarlowsz@outlook.com>
2021-05-10 09:42:20 -0300

    Mesclando a StatusAPI com a PlayerAPI.

--- src/com/github/caaarlowsz/basicpvp/player/PlayerAPI.java
+++ src/com/github/caaarlowsz/basicpvp/player/PlayerAPI.java
@@ -1,5 +1,6 @@
 package com.github.caaarlowsz.basicpvp.player;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
@@ -40,5 +41,29 @@ public final class PlayerAPI {
 
 	public static void removeUseReport(Player player) {
 		useReport.remove(player.getUniqueId());
+	}
+	
+	public static String getMoedas(Player player) {
+		return new DecimalFormat().format(StatusFile.getMoedas(player)).replace(",", ".");
+	}
+
+	public static Rank getRank(Player player) {
+		return Rank.getRankByXP(StatusFile.getXP(player));
+	}
+
+	public static String getXP(Player player) {
+		return new DecimalFormat().format(StatusFile.getXP(player)).replace(",", ".");
+	}
+
+	public static String getKillStreak(Player player) {
+		return new DecimalFormat().format(StatusFile.getKillStreak(player)).replace(",", ".");
+	}
+
+	public static String getAbates(Player player) {
+		return new DecimalFormat().format(StatusFile.getAbates(player)).replace(",", ".");
+	}
+
+	public static String getMortes(Player player) {
+		return new DecimalFormat().format(StatusFile.getMortes(player)).replace(",", ".");
 	}
 }

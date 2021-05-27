d9110aaac473fd0ebd5851ea64c1e65d9df9b6a3
Caaarlowsz <caaarlowsz@outlook.com>
2021-05-27 19:09:00 -0300

    Reestruturando o kit anchor.

--- src/com/github/caaarlowsz/basicpvp/kit/kits/AnchorKit.java
+++ src/com/github/caaarlowsz/basicpvp/kit/kits/AnchorKit.java
@@ -1,6 +1,5 @@
 package com.github.caaarlowsz.basicpvp.kit.kits;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,7 +7,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.util.Vector;
 
-import com.github.caaarlowsz.basicpvp.BasicKitPvP;
 import com.github.caaarlowsz.basicpvp.apis.WorldGuardAPI;
 import com.github.caaarlowsz.basicpvp.kit.Kit;
 import com.github.caaarlowsz.basicpvp.kit.KitAPI;
@@ -24,17 +22,12 @@ public final class AnchorKit extends Kit implements Listener {
 		if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
 			Player player = (Player) event.getEntity(), damager = (Player) event.getDamager();
 			if (WorldGuardAPI.hasPvP(player) && WorldGuardAPI.hasPvP(damager)) {
-				if (KitAPI.getKit(player) instanceof AnchorKit) {
+				if (KitAPI.getKit(player) instanceof AnchorKit || KitAPI.getKit(damager) instanceof AnchorKit) {
 					player.setVelocity(new Vector());
 					player.playSound(player.getLocation(), Sound.ANVIL_BREAK, 4F, 4F);
-					Bukkit.getScheduler().runTaskLater(BasicKitPvP.getInstance(),
-							() -> damager.setVelocity(new Vector()), 1L);
-				}
-				if (KitAPI.getKit(damager) instanceof AnchorKit) {
+
 					damager.setVelocity(new Vector());
 					damager.playSound(damager.getLocation(), Sound.ANVIL_BREAK, 4F, 4F);
-					Bukkit.getScheduler().runTaskLater(BasicKitPvP.getInstance(),
-							() -> player.setVelocity(new Vector()), 1L);
 				}
 			}
 		}

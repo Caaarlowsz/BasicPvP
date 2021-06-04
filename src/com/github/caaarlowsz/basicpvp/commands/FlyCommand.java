619d909f642307aaabac3801abf06185bc1c9621
Caaarlowsz <caaarlowsz@outlook.com>
2021-06-03 21:50:00 -0300

    Permitindo o uso do comando apenas no Spawn.

--- src/com/github/caaarlowsz/basicpvp/commands/FlyCommand.java
+++ src/com/github/caaarlowsz/basicpvp/commands/FlyCommand.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.github.caaarlowsz.basicpvp.utils.Strings;
+import com.github.caaarlowsz.basicpvp.warp.WarpAPI;
+import com.github.caaarlowsz.basicpvp.warp.warps.SpawnWarp;
 
 public final class FlyCommand implements CommandExecutor {
 
@@ -19,28 +21,35 @@ public final class FlyCommand implements CommandExecutor {
 					if (player.hasPermission("kitpvp.command.fly.others")) {
 						Player target = Bukkit.getPlayer(args[0]);
 						if (target != null) {
-							if (!target.getAllowFlight()) {
-								target.setAllowFlight(true);
-								player.sendMessage(
-										Strings.getPrefixo() + " §aVocê habilitou o Fly de " + target.getName() + ".");
-							} else {
-								player.setAllowFlight(false);
-								player.sendMessage(Strings.getPrefixo() + " §cVocê desabilitou o Fly de "
-										+ target.getName() + ".");
-							}
+							if (WarpAPI.getWarp(target) instanceof SpawnWarp) {
+								if (!target.getAllowFlight()) {
+									target.setAllowFlight(true);
+									player.sendMessage(Strings.getPrefixo() + " §aVocê habilitou o Fly de "
+											+ target.getName() + ".");
+								} else {
+									player.setAllowFlight(false);
+									player.sendMessage(Strings.getPrefixo() + " §cVocê desabilitou o Fly de "
+											+ target.getName() + ".");
+								}
+							} else
+								player.sendMessage(Strings.getPrefixo() + " §c" + target.getName()
+										+ " precisa estar no Spawn para voar.");
 						} else
 							player.sendMessage(Strings.getPrefixo() + " §cO seu alvo não foi encontrado");
 					} else
 						player.sendMessage(Strings.getPrefixo()
 								+ " §cVocê não possui Permissão para habilitar o Fly de outros jogadores.");
 				} else {
-					if (!player.getAllowFlight()) {
-						player.setAllowFlight(true);
-						player.sendMessage(Strings.getPrefixo() + " §aVocê habilitou seu Fly.");
-					} else {
-						player.setAllowFlight(false);
-						player.sendMessage(Strings.getPrefixo() + " §cVocê desabilitou seu Fly.");
-					}
+					if (WarpAPI.getWarp(player) instanceof SpawnWarp) {
+						if (!player.getAllowFlight()) {
+							player.setAllowFlight(true);
+							player.sendMessage(Strings.getPrefixo() + " §aVocê habilitou seu Fly.");
+						} else {
+							player.setAllowFlight(false);
+							player.sendMessage(Strings.getPrefixo() + " §cVocê desabilitou seu Fly.");
+						}
+					} else
+						player.sendMessage(Strings.getPrefixo() + " §cVocê precisa estar no Spawn para voar.");
 				}
 			} else
 				player.sendMessage(Strings.getPrefixo() + " §cVocê não possui Permissão para usar este Comando.");
